00cd70c19f54855b1f94cdfc2d5085a7917e5032
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2022-02-23 23:05:53 +0300

    Button update

---
+++ src/UI/Button/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Button extends Component{
+    render(){
+        const{children, submit, copied}=this.props;
+
+        return (
+        <button
+                className={`btn${copied ? 'copied' : ''}`}
+                type={submit ? 'submit' :'button'}
+                disabled={copied}
+
+
+        >
+            {children}
+            </button>
+            );
+            
+        }
+    }
+
+export default Button;
